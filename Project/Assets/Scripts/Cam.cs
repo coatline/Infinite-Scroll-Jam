86df4397f3206785ea70b2844a17be6803e87079
Bryler <bryler.castle@gmail.com>
2024-08-30 08:58:05 -0400

    Moved Project Root Folder

---
+++ Project/Assets/Scripts/Cam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float followSpeed = 1;
+
+    void LateUpdate()
+    {
+        if (!player) { return; }
+        if (transform.position.y < player.transform.position.y - 2f)
+        {
+            transform.position = Vector3.Slerp(transform.position, new Vector3(0, player.transform.position.y, -10), Time.deltaTime * followSpeed);
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public bool bouncedOn;
+    public bool keepX;
+
+    private void Awake()
+    {
+        if (keepX) { return; }
+        transform.position = new Vector3(Random.Range(-8f, 8f), transform.position.y);
+    }
+}
